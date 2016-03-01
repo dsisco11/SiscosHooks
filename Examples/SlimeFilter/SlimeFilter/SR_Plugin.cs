c00ba49c72d0cdf8bef1ceb2e84872835493b55e
dsisco11 <dsisco11@gmail.com>
2016-03-01 03:55:59 -0600

    Added examples

---
+++ Examples/SlimeFilter/SlimeFilter/SR_Plugin.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using SR_PluginLoader;
+
+
+namespace SlimeFilter
+{
+    public static class SR_Plugin
+    {
+        /*
+        THIS EXAMPLE REQUIRES THAT THE USER HAVE 'SiscosHooks' INSTALLED IN ORDER TO RUN!
+        */
+        public static Plugin_Data PLUGIN_INFO = new Plugin_Data()
+        {
+            NAME = "SlimeFilter",
+            DESCRIPTION = @"- Holding the ALT key will make the VacPak's suction ignore slimes.",
+            VERSION = new Plugin_Version(1, 0)// v1.0
+        };
+
+        private static GameObject root;
+
+        public static void Load()
+        {
+            SR_Plugin.root = new GameObject("SlimeFilterMod");
+            SR_Plugin.root.AddComponent<SlimeFilter>();
+
+            UnityEngine.Object.DontDestroyOnLoad(SR_Plugin.root);
+        }
+
+        public static void Unload()
+        {
+            UnityEngine.Object.Destroy(SR_Plugin.root);
+        }
+
+    }
+}
---
+++ Examples/SlimeFilter/SlimeFilter/SlimeFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngineInternal;
+using SiscosHooks;
+using SR_PluginLoader;
+
+namespace SlimeFilter
+{
+    class SlimeFilter : MonoBehaviour
+    {
+        public void Awake()
+        {
+            SiscoHook.register(this, HOOK_ID.Vac_Can_Capture, this.Vac_Can_Capture);
+        }
+
+        private Sisco_Return Vac_Can_Capture(ref object sender, ref object[] args)
+        {
+            if (!Input.GetKey(KeyCode.LeftAlt) && !Input.GetKey(KeyCode.RightAlt)) return null;
+            Vacuumable vac = (Vacuumable)sender;
+            SlimeFaceAnimator anim = vac.GetComponent<SlimeFaceAnimator>();
+            if (anim != null)
+            {
+                // Here we are telling the hook to make the root function which fired it, to return FALSE
+                // in this case returning false from said function tells the game that this object cannot be vacuumed up!
+                return new Sisco_Return() { has_custom_return = true, return_value = false };
+            }
+
+            return null;
+        }
+        
+        private bool findMainMenuUI()
+        {
+            return (UnityEngine.Object.FindObjectOfType<MainMenuUI>().gameObject != null);
+        }
+    }
+}
